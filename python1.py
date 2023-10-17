0f9e10ffc0161f438658a11e2a57433f89e1e894
pfb08 <pfb08@info08.cshl.edu>
2023-10-17 12:08:24 -0400

    new

--- python1.py
+++ python1.py
@@ -2,9 +2,9 @@
 print ("My name: Camila")
 print ("occupation: postdoc")
 print ("favorite color: green")
-fav_act = "listening to music"
+fav_act = "listening to music" #defines favorite activity
 print ("favorite activity:" , fav_act)
-fav_food = "pizza"
-print ("favorite food:" , fav_food)
-fav_animal = "elephant"
+fav_food = "pizza" #defines favorite food
+print (f"favorite food: {fav_food}")
+fav_animal = "elephant" #defines favorite animal
 print ("favorite animal:" , fav_animal)
