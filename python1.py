5642a02e3bd044bcd6a501aed27e4bf2e4783590
pfb08 <pfb08@info08.cshl.edu>
2023-10-17 11:57:37 -0400

    updated

--- python1.py
+++ python1.py
@@ -2,3 +2,9 @@
 print ("My name: Camila")
 print ("occupation: postdoc")
 print ("favorite color: green")
+fav_act = "listening to music"
+print ("favorite activity:" , fav_act)
+fav_food = "pizza"
+print ("favorite food:" , fav_food)
+fav_animal = "elephant"
+print ("favorite animal:" , fav_animal)
