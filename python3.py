3c179919f6a94a528c498ccdb1996b91e7ee6bc2
pfb08 <pfb08@info08.cshl.edu>
2023-10-17 16:30:07 -0400

    completed

--- python3.py
+++ python3.py
@@ -1,8 +1,22 @@
 #!/usr/bin/env python3
-count = 50
+count = 57 
 if count > 0:
- print ('positive')
+	print ('positive')
+	if count < 50:
+		print ('is smaller than 50')
+		if (count % 2) == 0:
+			print ('it is an even number')
+	if count > 50:
+		print ('is bigger than 50')
+		if (count % 2) == 0:
+			print ('it is divisible by 3')			
+		else:
+			print ('it is not divisible by 3')
+	if count == 50:
+		print ('this is 50!')
+
 elif count == 0:
- print ('count equals 0')
+	print ('count equals 0')
+
 else:
- print ('negative') 
+	print ('negative') 
