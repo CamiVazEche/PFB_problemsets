49b7959a50777a85e73e000ed9f7ed507d09a7da
pfb08 <pfb08@info08.cshl.edu>
2023-10-17 12:35:23 -0400

    completed

--- python1.py
+++ python1.py
@@ -1,13 +1,19 @@
 #!/usr/bin/env python3
+import sys
 print ("My name: Camila")
 print ("occupation: postdoc")
 print ("favorite color: green")
 
 fav_act = "listening to music" #defines favorite activity
-	print ("favorite activity:" , fav_act)
+print ("favorite activity:" , fav_act)
 
 fav_food = "pizza" #defines favorite food
-	print (f"favorite food: {fav_food}")
+print (f"favorite food: {fav_food}")
 
 fav_animal = "elephant" #defines favorite animal
-	print ("favorite animal:" , fav_animal)
+print ("favorite animal:" , fav_animal)
+
+fav_act = sys.argv[1]
+fav_food = sys.argv[2]
+fav_animal = sys.argv[3]
+print('Favorite activity:', fav_act, 'Favorite food:', fav_food, 'Favorite animal:', fav_animal)
