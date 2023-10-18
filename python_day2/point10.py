da0174bf4ac17e8ff2edbd1a757ff8e6ee26c90a
pfb08 <pfb08@info08.cshl.edu>
2023-10-18 16:20:02 -0400

    completed

---
+++ python_day2/point10.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+import sys
+
+#task for point10
+
+dna_seq = sys.argv[1]
+sub_dna = dna_seq[99:201]
+sub_count = sub_dna.count('G')
+
+print('Begin')
+print (sub_dna)
+print('End')
+print (sub_count)
