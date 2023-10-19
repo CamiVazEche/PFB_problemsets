317c529441cec0950c213bd6119828dc35d05d65
pfb08 <pfb08@info08.cshl.edu>
2023-10-19 16:45:49 -0400

    completed

---
+++ python_day2/4point8.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+
+numbers = range(100)
+for number in numbers:
+	number +=1
+	print(number)
---
+++ python_day2/4point9.py
@@ -0,0 +1,4 @@
+#!/usr/bin/env python3
+
+numbers = range(100)
+range_nums = [print(number +1) for number in numbers]
