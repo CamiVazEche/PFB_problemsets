8bfa6e0d32ef65be9d38f387fa7f523fbc07a992
pfb08 <pfb08@info08.cshl.edu>
2023-10-18 20:30:07 -0400

    delete

--- python_day2/point14.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-import sys
-
-DNA_seq = sys.argv[1]
-Eco_start_dig = DNA_seq.find('GAATTC')
-Eco_start = (Eco_start_dig+1)
-Eco_end = (Eco_start+5)
-
-print(f'EcoRI startPos:{Eco_start} endPos:{Eco_end}')
