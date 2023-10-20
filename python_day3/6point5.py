1247b4cae5b88ba496a9e3027bcbdae3d2a5942a
pfb08 <pfb08@info08.cshl.edu>
2023-10-20 11:27:59 -0400

    to be completed

---
+++ python_day3/6point5.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+#trying out reading, writing and saving files
+#file to open from shell = https://raw.githubusercontent.com/prog4biol/pfb2023/master/files/Python_06.txt
+
+import sys
+
+poem = open(sys.argv[1],"r")
+
+
